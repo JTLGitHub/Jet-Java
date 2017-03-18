69b7bd1141fd38402ec7012b21d66c833705f5ec
JTLGitHub <jitendra.t.lad@gmail.com>
2017-03-17 21:56:37 -0700

    Third Commit

--- src/pkg_string/cls_string.java
+++ src/pkg_string/cls_string.java
@@ -3,9 +3,17 @@ package pkg_string;
 public class cls_string {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub  hi
 		System.out.println("This is First Line");
 		System.out.println("This is Second Line");
+		System.out.println("This is Third Line");
 	}
 
+
+	public static void main1(String[] args) {
+		// TODO Auto-generated method stub  hi
+		System.out.println("This is First Line");
+		System.out.println("This is Second Line");
+		System.out.println("This is Third Line");
+	}
 }
