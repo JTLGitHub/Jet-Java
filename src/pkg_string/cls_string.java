cb0e9f2402c544c76f698022060d17201cc71c55
JTLGitHub <jitendra.t.lad@gmail.com>
2017-03-17 19:36:53 -0700

    Second Commit

--- src/pkg_string/cls_string.java
+++ src/pkg_string/cls_string.java
@@ -5,6 +5,7 @@ public class cls_string {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is First Line");
+		System.out.println("This is Second Line");
 	}
 
 }
